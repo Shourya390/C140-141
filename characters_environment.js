b08f7a77bf8815d0feb80f0f2e35bd9977d69e7a
Shourya390 <77324733+Shourya390@users.noreply.github.com>
2021-03-25 22:21:49 +0530

    Add files via upload

---
+++ characters_environment.js
@@ -0,0 +1,14 @@
+/*=================================
+=            Variables            =
+=================================*/
+
+/* main character variabes */
+var mario, bricks,clouds,mountains,enemyMushrooms,pipes,platforms,coins;
+
+/* Control variabes */
+var control={
+  up: "UP_ARROW", // 32=spaceBar
+  left: 'LEFT_ARROW',
+  right: 'RIGHT_ARROW',
+  revive: 32
+}
